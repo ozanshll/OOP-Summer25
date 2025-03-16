1fb68f868e39f79df28f2a6a029fd45bca3a85c7
ozanshll <116595268+ozanshll@users.noreply.github.com>
2025-03-16 15:09:15 +0000

    Add new file

---
+++ student_details_structure.py
@@ -0,0 +1,10 @@
+student1 = {
+    "first_name": "Ozan",
+    "last_name": "Sahillioğulları",
+    "index_number": 35441,
+    "nationality": "Turkish",
+    "starting_date": "2025-03-16",
+    "courses": ["Object Oriented Programming", "Data Structures", "Algorithms"]
+}
+
+print(student1["first_name"])
