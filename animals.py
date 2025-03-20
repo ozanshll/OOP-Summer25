877d171b48009a3fc62c2c62e604f2c736266563
ozanshll <116595268+ozanshll@users.noreply.github.com>
2025-03-20 13:05:44 +0000

    Add Person class

---
+++ animals.py
@@ -0,0 +1,36 @@
+animals = [
+    {
+        'name': 'Cat',
+        'group': 'Mammals',
+        'number_of_legs': 4,
+        'skills': ['jumping', 'climbing', 'hunting']
+    },
+    {
+        'name': 'Eagle',
+        'group': 'Birds',
+        'number_of_legs': 2,
+        'skills': ['flying', 'hunting', 'sharp vision']
+    },
+    {
+        'name': 'Frog',
+        'group': 'Amphibians',
+        'number_of_legs': 4,
+        'skills': ['jumping', 'swimming']
+    },
+    {
+        'name': 'Shark',
+        'group': 'Fish',
+        'number_of_legs': 0,
+        'skills': ['swimming', 'hunting']
+    },
+    {
+        'name': 'Snake',
+        'group': 'Reptiles',
+        'number_of_legs': 0,
+        'skills': ['slithering', 'camouflage']
+    }
+]
+
+# Printing the dictionaries
+for animal in animals:
+    print(animal)
