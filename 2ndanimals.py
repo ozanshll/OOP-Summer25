eb3a6d0f3033bd822fa39c9b6ad6e044712dc0d6
ozanshll <116595268+ozanshll@users.noreply.github.com>
2025-03-20 13:58:08 +0000

    Add Person class

--- 2ndanimals.py
+++ 2ndanimals.py
@@ -1,29 +1,29 @@
 # animal.py
 
 class Animal:
-    def __init__(self, name: str, group: str, number_of_legs: int, skills: list):
+    def __init__(self, name: str, group: str, enviroment: str, skills: list):
         """
         Initialize an Animal object with its name, group, number of legs, and skills.
         """
         self.name = name
         self.group = group
-        self.number_of_legs = number_of_legs
+        self.enviroment = enviroment 
         self.skills = skills
 
     def __str__(self):
         """
         Return a string representation of the Animal object.
         """
-        return f"Animal(Name: {self.name}, Group: {self.group}, Legs: {self.number_of_legs}, Skills: {', '.join(self.skills)})"
+        return f"Animal(Name: {self.name}, Group: {self.group}, Legs: {self.enviroment}, Skills: {', '.join(self.skills)})"
 
 
 # Creating instances of the Animal class
 animals = [
-    Animal('Cat', 'Mammals', 4, ['jumping', 'climbing', 'hunting']),
-    Animal('Eagle', 'Birds', 2, ['flying', 'hunting', 'sharp vision']),
-    Animal('Frog', 'Amphibians', 4, ['jumping', 'swimming']),
-    Animal('Shark', 'Fish', 0, ['swimming', 'hunting']),
-    Animal('Snake', 'Reptiles', 0, ['slithering', 'camouflage'])
+    Animal('Cat', 'Mammals', 'land' , ['jumping', 'climbing', 'hunting']),
+    Animal('Eagle', 'Birds', 'sky', ['flying', 'hunting', 'sharp vision']),
+    Animal('Frog', 'Amphibians', 'land', ['jumping', 'swimming']),
+    Animal('Shark', 'Fish', 'sea', ['swimming', 'hunting']),
+    Animal('Snake', 'Reptiles', 'land', ['slithering', 'camouflage'])
 ]
 
 # Printing all animals
